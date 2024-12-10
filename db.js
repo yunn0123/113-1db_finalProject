eec405e2b018d7b370b81cb4d4244cdf50a8b910
yunn0123 <fefe20030123@gmail.com>
2024-12-11 01:16:02 +0800

    update db.js notice

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'db_proj',
-  password: '0000',
+  database: database_name, // 自行輸入本地端資料庫名稱
+  password: password, // 自行輸入本地端資料庫密碼
   port: 5432, // 預設 PostgreSQL 埠
 });
 
